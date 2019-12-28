4c00e0a13eb3023e4c48811427d3d1a7132cbf50
Alex Puzyk <APuzyk@gmail.com>
2019-12-28 11:37:22 -0800

    Updated to not be recursive because python still need to figure out how to get ordering to work

--- scc.py
+++ scc.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 file = '/home/apuzyk/Documents/algorithms_coursera/SCC.txt'
 
 with open(file) as f:
@@ -6,66 +8,63 @@ with open(file) as f:
 o = [i.rstrip().split(' ') for i in o]
 o = [i for i in o if i[0] != i[1]]
 
-nodes = [j for i in o for j in i]
-nodes = list(set(nodes))
+#index at 0 to make things easier
+nodes = list(set([j for i in o for j in i]))
 len(nodes)
-g = {}
+nodes = [i for i in range(len(nodes))]
+g = [[] for i in range(len(nodes))]
+g_r = [[] for i in range(len(nodes))]
+
 for i in o:
-    if g.get(i[0]) is None:
-        g[i[0]] = [i[1]]
-    else:
-        g[i[0]].append(i[1])
-g_rev = {}
-for i in o:
-    if g_rev.get(i[1]) is None:
-        g[i[1]] = [i[0]]
-    else:
-        g[i[1]].append(i[0])
+    g[int(i[0]) - 1].append(int(i[1]) - 1)
+    g_r[int(i[1]) - 1].append(int(i[0]) - 1)
 
-explore_list = [0 for i in nodes]
-f_s = {}
-for i in nodes:
-    f_s[i] = 999999999
-curLabel = len(nodes)
+visited = [False] * len(nodes)
+order = []
 
+# Iterate through nodes
+for node in nodes:
+    #If a node isn't visited go through it
+    if not visited[node]:
+        # Create a stack for DFS and init with node
+        stack = deque()
+        stack.append(node)
+        while len(stack) > 0:
+            # Pop from the top of the stack (last one in)
+            # for first iter it'll be our first node
+            # otherwise it's last one we've visited
+            v = stack.pop()
+            if not visited[v]:
+                # IF we haven't been to the node
+                # set visited to true and append each edge to the list
+                # that we haven't been to yet
+                #TODO how do we figure out when to label the thing
+                visited[v] = True
+                for w in g_r[v]:
+                    if not visited[w]:
+                        stack.append(w)
+            #
+            order.append(v)
 
-def dfs_topo(g, s):
-    global curLabel
-    global f_s
+order.reverse()
 
-    explore_list[int(s)-1] = 1
-    for i in g[s]:
-        if explore_list[int(i)-1] == 0:
-            dfs_topo(g, i)
-    f_s[curLabel] = s
-    curLabel = curLabel-1
+visited = [False] * len(nodes)
 
-#get order for grev:
-dfs_topo(g_rev, '1')
-
-#create new explore list for g
-explore_list = [0 for i in nodes]
-
-#scc tracking
-scc = {}
+scc = []
 num_scc = 0
-for i in range(len(nodes)):
-    v = f_s[i]
-    if explore_list[int(v) - 1] == 0:
-        num_scc += 1
-        dfs_scc(g, v)
+stack = deque()
+for v in order:
+    if not visited[v]:
+        scc.append([])
+        stack.append(v)
+        while len(stack) > 0:
+            w = stack.pop()
+            if not visited[w]:
+                visited[w] = True
+                scc[len(scc) - 1].append(w)
+                for u in g[w]:
+                    stack.append(u)
 
-
-def dfs_scc(g, s):
-    global explore_list
-    global num_scc
-    global scc
-
-    explore_list[int(s) - 1] = 1
-    if scc.get(num_scc) is None:
-        scc[num_scc] = [s]
-    else:
-        scc[num_scc].append(s)
-    for v in g[s]:
-        if explore_list[int(v) - 1] == 0:
-            dfs_scc(g, v)
+o = [len(i) for i in scc]
+o.sort(reverse=True)
+o
